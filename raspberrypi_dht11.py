833dd2f37cebac6235589c3a9e2c24927bf2fc63
maurizioarrigoni <maurizio.arrigoni@tiscali.it>
2024-06-22 12:01:45 +0200

    .

--- raspberrypi_dht11.py
+++ raspberrypi_dht11.py
@@ -9,17 +9,21 @@ import sys
 import Adafruit_DHT
 import time
 from simple_http_server import route, server
+from datetime import datetime
 
 @route("/")
 def index():
     sensor = 11
     gpio = 4
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")    
     humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
     #response = 'Temperatura: {0:0.1f}*C'.format(temperature, humidity) +'</br>' + 'Umidità: {1:0.1f} %'.format(temperature, humidity)
     HTML_CONTENT = """<!DOCTYPE html><html lang="it"><head><meta charset="UTF-8"><title></title></head><body>"""
     HTML_CONTENT = HTML_CONTENT + """<h1>raspi3 with DHT11 temperature & humidity sensor</h1>"""
     HTML_CONTENT = HTML_CONTENT + """<h1>Temperatura: """ + '{0:0.1f}*C'.format(temperature, humidity) + """</h1>"""
     HTML_CONTENT = HTML_CONTENT + """<h1>Umidità: """ + '{1:0.1f} %'.format(temperature, humidity) + """</h1>"""
+    HTML_CONTENT = HTML_CONTENT + """<h2>""" + date_time + """</h2>"""
     HTML_CONTENT = HTML_CONTENT + """</body></html>"""
     return (HTML_CONTENT)
 
